4af934cb19e8f1c4e76d63d60dc970a52c322905
Kendall Easterly <qfletcher89@gmail.com>
2022-01-14 23:31:45 -0600

    completed flash contract

--- flash/hardhat.config.js
+++ flash/hardhat.config.js
@@ -2,8 +2,10 @@
  * @type import('hardhat/config').HardhatUserConfig
  */
 require("@nomiclabs/hardhat-ethers");
+const dotenv = require("dotenv")
+dotenv.config()
 module.exports = {
-	solidity: "0.7.3",
+	solidity: "0.7.6",
 	networks: {
 		polygon: {
 			url: "https://polygon-mainnet.infura.io/v3/ad4b7eeae04e442ca372d113293e0609",
