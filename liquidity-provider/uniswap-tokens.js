422df1d31c00e1102a7ee21b75e1a6247766a9d5
Kendall Easterly <qfletcher89@gmail.com>
2022-02-09 01:03:38 -0600

    go through minting, awaiting interface method

---
+++ liquidity-provider/uniswap-tokens.js
@@ -0,0 +1,32 @@
+const { Token } = require("@uniswap/sdk-core")
+
+const WETH = new Token(
+	137,
+	"0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619",
+	18,
+	"WETH",
+	"Wrapped Ether"
+)
+const MATIC = new Token(
+	137,
+	"0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270",
+	18,
+	"WMATIC",
+	"Wrapped Matic"
+)
+const DAI = new Token(
+	137,
+	"0x8f3cf7ad23cd3cadbd9735aff958023239c6a063",
+	18,
+	"DAI",
+	"Dai Stablecoin"
+)
+const USDT = new Token(
+	137,
+	"0xc2132d05d31c914a87c6611c10748aeb04b58e8f",
+	6,
+	"USDT",
+	"Tether USD"
+)
+
+module.exports = {WETH, MATIC, DAI, USDT}
