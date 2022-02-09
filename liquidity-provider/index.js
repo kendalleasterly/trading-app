a012c21fd3b436bcf9c805bc7af4f1457baea321
Kendall Easterly <qfletcher89@gmail.com>
2022-02-09 01:03:09 -0600

    got to minting phase

--- liquidity-provider/index.js
+++ liquidity-provider/index.js
@@ -1,12 +1,17 @@
 const app = require("express")()
 
 const dotenv = require("dotenv")
-const { getInfo, getPool } = require("./uniswap")
+const { getInfo, getPool, mintPosition } = require("./uniswap")
 dotenv.config()
 
 const port = process.env.PORT || 4000
 
 app.get("/info", (req, res) => {
+    res.send("")
+})
+
+app.post("/mint", (req, res) => {
+    mintPosition()
     res.send("")
 })
 
--- liquidity-provider/uniswap.js
+++ liquidity-provider/uniswap.js
@@ -1,24 +1,27 @@
-const {default: axios} = require("axios");
-const {Position, Pool} = require("@uniswap/v3-sdk");
-const { Token } = require("@uniswap/sdk-core");
-const {abi: IUniswapV3PoolABI} = require("@uniswap/v3-core/artifacts/contracts/interfaces/IUniswapV3Pool.sol/IUniswapV3Pool.json"); 
-const { ethers } = require("ethers");
+const { default: axios } = require("axios")
+const { Position, Pool, nearestUsableTick, NonfungiblePositionManager } = require("@uniswap/v3-sdk")
+const { ethers, BigNumber } = require("ethers")
+const {USDT, DAI} = require("./uniswap-tokens")
+const dotenv = require("dotenv")
+const { Percent } = require("@uniswap/sdk-core")
+const {abi: INonfungiblePositionManagerABI} = require("@uniswap/v3-periphery/artifacts/contracts/interfaces/INonfungiblePositionManager.sol/INonfungiblePositionManager.json")
 
-const apiURL = "https://api.thegraph.com/subgraphs/name/ianlapham/uniswap-v3-polygon";
+const {
+	abi: IUniswapV3PoolABI,
+} = require("@uniswap/v3-core/artifacts/contracts/interfaces/IUniswapV3Pool.sol/IUniswapV3Pool.json")
 
-const WETH = new Token(137, "0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619", 18, "WETH", "Wrapped Ether");
-const MATIC = new Token(137, "0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270", 18, "WMATIC", "Wrapped Matic");
 
- const provider = new ethers.providers.JsonRpcProvider(process.env.INFURA_URL);
- const poolAddress = "0x167384319b41f7094e62f7506409eb38079abff8";
- const poolContract = new ethers.Contract(
-		poolAddress,
-		IUniswapV3PoolABI,
-		provider
- );
+dotenv.config()
+
+const apiURL =
+	"https://api.thegraph.com/subgraphs/name/ianlapham/uniswap-v3-polygon"
+
+const provider = new ethers.providers.JsonRpcProvider(process.env.INFURA_URL)
+let wallet = new ethers.Wallet(process.env.PRIVATE_KEY)
+wallet = wallet.connect(provider)
 
 function getInfo() {
-	const positionID = 45010;
+	const positionID = 45010
 
 	const query = `
   {
@@ -33,56 +36,102 @@ function getInfo() {
         token1Price
       }
     }
-  }`;
+  }`
 
-	axios.post(apiURL, {query}).then((response) => {
-		const position = response.data.data.position;
+	axios.post(apiURL, { query }).then((response) => {
+		const position = response.data.data.position
 
-		const currentPrice = position.pool.token1Price;
-		const lowerBound = new Number(position.tickLower.price0);
-		const upperBound = new Number(position.tickUpper.price0);
+		const currentPrice = position.pool.token1Price
+		const lowerBound = new Number(position.tickLower.price0)
+		const upperBound = new Number(position.tickUpper.price0)
 
-		const localHighest = upperBound - lowerBound;
-		const localPrice = currentPrice - lowerBound;
+		const localHighest = upperBound - lowerBound
+		const localPrice = currentPrice - lowerBound
 
-		const percentage = localPrice / localHighest;
+		const percentage = localPrice / localHighest
 
 		if (percentage <= 0.275 || percentage >= 0.725) {
 			//create a new position
 		} else {
-			console.log("good range!");
+			console.log("good range!")
 		}
 
-		console.log(percentage);
-	});
+		console.log(percentage)
+	})
 }
 
 async function mintPosition() {
+	const poolAddress = "0x42f0530351471dab7ec968476d19bd36af9ec52d"
+	const [POOL, immutables, state] = await getPool(USDT, DAI, poolAddress)
+	
+	const position = new Position({
+		pool: POOL,
+		liquidity: state.liquidity * 0.0002,
+		tickLower: nearestUsableTick(state.tick, immutables.tickSpacing) - immutables.tickSpacing * 10,
+		tickUpper: nearestUsableTick(state.tick, immutables.tickSpacing) + immutables.tickSpacing * 10
+	})
 
-  const MATIC_WETH_POOL = await getPool()
+	const blockNumber =  await wallet.provider.getBlockNumber()
+
+	const data = {
+		slippageTolerance: new Percent(50, 1000),
+		recipient: wallet.address,
+		deadline: blockNumber + 200
+	}
+
+	const {calldata, value} = NonfungiblePositionManager.addCallParameters(position, data)
+
+	const NFPManagerAddress = "0xC36442b4a4522E871399CD717aBDD847Ab11FE88"
+	// const NFPManager = new ethers.Contract(
+	// 	NFPManagerAddress,
+	// 	INonfungiblePositionManagerABI,
+	// 	wallet
+	// )
+
+	const gasPrice = await provider.getGasPrice()
+	const nonce = await wallet.getTransactionCount()
+	console.log(nonce)
+
+	const tx = await wallet.sendTransaction({
+		from: wallet.address,
+		to: NFPManagerAddress,
+		value: value,
+		data: calldata,
+		gasPrice: gasPrice,
+		nonce: nonce,
+		gasLimit: 2100000
+	})
+
+	console.log(tx)
 }
 
 //MARK: Helpers
 
-async function getPool() {
+async function getPool(tokenA, tokenB, poolAddress) {
 	const [immutables, state] = await Promise.all([
-		getPoolImmutables(),
-		getPoolState(),
-	]);
+		getPoolImmutables(poolAddress),
+		getPoolState(poolAddress),
+	])
 
-	const MATIC_WETH_POOL = new Pool(
-		WETH,
-		MATIC,
+	const POOL = new Pool(
+		tokenA,
+		tokenB,
 		immutables.fee,
 		state.sqrtPriceX96.toString(),
 		state.liquidity.toString(),
 		state.tick
-	);
-	
-  return MATIC_WETH_POOL
+	)
+
+	return [POOL, immutables, state]
 }
 
-async function getPoolImmutables() {
+async function getPoolImmutables(poolAddress) {
+
+	const poolContract = new ethers.Contract(
+		poolAddress,
+		IUniswapV3PoolABI,
+		provider
+	)
 
 	const [factory, token0, token1, fee, tickSpacing, maxLiquidityPerTick] =
 		await Promise.all([
@@ -92,7 +141,7 @@ async function getPoolImmutables() {
 			poolContract.fee(),
 			poolContract.tickSpacing(),
 			poolContract.maxLiquidityPerTick(),
-		]);
+		])
 
 	const immutables = {
 		factory,
@@ -101,15 +150,22 @@ async function getPoolImmutables() {
 		fee,
 		tickSpacing,
 		maxLiquidityPerTick,
-	};
-	return immutables;
+	}
+	return immutables
 }
 
-async function getPoolState() {
+async function getPoolState(poolAddress) {
+
+	const poolContract = new ethers.Contract(
+		poolAddress,
+		IUniswapV3PoolABI,
+		provider
+	)
+
 	const [liquidity, slot] = await Promise.all([
 		poolContract.liquidity(),
 		poolContract.slot0(),
-	]);
+	])
 
 	const PoolState = {
 		liquidity,
@@ -120,8 +176,8 @@ async function getPoolState() {
 		observationCardinalityNext: slot[4],
 		feeProtocol: slot[5],
 		unlocked: slot[6],
-	};
-	return PoolState;
+	}
+	return PoolState
 }
 
-module.exports = {getInfo, mintPosition};
+module.exports = { getInfo, mintPosition }
